1ab8998fc62b5f9335688c4ba757ace97f3a95ee
avenInk <aven132ink@gmail.com>
2026-01-14 00:12:57 -0500

    Se anexa pagina de menú despues de iniciar sesion, pendiente interaccion con el menú.

--- script/script_login.js
+++ script/script_login.js
@@ -1,4 +1,13 @@
 
+async function login_page_disabled(){
+    const login_page = document.getElementById("login-page");
+    login_page.style.display = "none";
+} 
+
+async function menu_page_enabled(){
+    const menu_page = document.getElementById("menu-page");
+    menu_page.style.display = "block";
+}
 
 async function get_work_directory() {
     try {
@@ -13,29 +22,60 @@ async function login_button_activate(){
 
 
 
+    const login_form = document.getElementById("login-form")
     const login_button = document.getElementById("sig-in-button")
 
+    login_form.addEventListener("submit", async (event)=>{
+        login_button.disabled = true;
+
+        try{
+            event.preventDefault();
+            console.log("Se ha oprimido el boton de login");
+        }
+        catch(err){
+            console.error("Error al oprimir el boton de login:", err);
+        }
+        finally{
+
+            login_button.disabled = false;
+            console.log("Login button habilitado nuevamente");
+
+            await login_page_disabled();
+            await menu_page_enabled();
+
+        }
+
+    })
+
     if (window.folderHandle){
-        login_button.classList.add("active-login-button")
+         login_button.classList.add("active-login-button")
     }
 
-    
+
+
 }
 
 
 function main(){
 
-    const working_folder = document.getElementById("select-work-folder")
-    working_folder.addEventListener("click", async ()=>{
-        working_folder.disabled = true;
+    const working_folder_button = document.getElementById("select-work-folder")
+
+
+
+    working_folder_button.addEventListener("click", async ()=>{
+        working_folder_button.disabled = true;
         console.log("Se ha oprimido el boton de seleccionar la carpeta");
         await get_work_directory();
 
-        login_button_activate();
+        await login_button_activate(); // Activar el boton de login si se selecciono una carpeta
 
-        working_folder.disabled = false;
+        if(window.folderHandle){
+            console.log("Se ha seleccionado la carpeta de trabajo");
+        }
+
+        working_folder_button.disabled = false;
     })
-    
+
 }
 
 main()
